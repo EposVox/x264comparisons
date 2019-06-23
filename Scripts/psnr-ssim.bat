24fb667807be4b8752763598228b5692d18b6f78
Adam Taylor <eposvox@gmail.com>
2019-06-23 16:04:36 -0400

    Delete psnr-ssim.bat

--- Scripts/psnr-ssim.bat
+++
@@ -1,7 +0,0 @@
-for /R %%A in (*.mp4) do (
-    echo Processing %%A
-	ffmpeg -i lossless.mov -i "%%A" -lavfi  psnr=psnr.log -f null - > "%%~dnpA-psnr.txt"
-	ffmpeg -i lossless.mov -i "%%A" -lavfi  ssim=ssim.log -f null - > "%%~dnpA-SSIM.txt"
-    echo Processed %%A
-)
-pause
